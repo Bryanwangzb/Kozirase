d00a802cbf8767b9bf91242f8ce35a6b491ab92c
wang <test@gmail.com>
2020-11-13 10:33:46 +0900

    add json place hosder api interface.

--- app/src/main/java/com/kozirase/app/JsonPlaceHolderApi.java
+++ app/src/main/java/com/kozirase/app/JsonPlaceHolderApi.java
@@ -1,8 +1,20 @@
 package com.kozirase.app;
 
+import java.lang.invoke.CallSite;
+import java.util.List;
+
+import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface JsonPlaceHolderApi {
+    @GET("posts")
+    Call<List<Post>> getPosts(
+            @Query("userId") Integer[] userId,
+            @Query("_sort") String sort,
+            @Query("_order") String order
+    );
+
 
 
 }
