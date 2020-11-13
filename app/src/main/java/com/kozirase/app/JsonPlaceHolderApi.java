8ca11920db98485ec929b88aede47676086ce73b
wang <test@gmail.com>
2020-11-13 10:36:58 +0900

    add 3 types get interfaces

--- app/src/main/java/com/kozirase/app/JsonPlaceHolderApi.java
+++ app/src/main/java/com/kozirase/app/JsonPlaceHolderApi.java
@@ -2,10 +2,13 @@ package com.kozirase.app;
 
 import java.lang.invoke.CallSite;
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
 
 public interface JsonPlaceHolderApi {
     @GET("posts")
@@ -15,6 +18,11 @@ public interface JsonPlaceHolderApi {
             @Query("_order") String order
     );
 
+    @GET("posts")
+    Call<List<Post>> getPosts(@QueryMap Map<String,String> parameters);
+
+    @GET
+    Call<Post> createPost(@Body Post post);
 
 
 }
