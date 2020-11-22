f09d8fb4b72da9ff28ccd115c67d4075758233d2
Bryanwangzb <bryanwzb@163.com>
2020-11-22 21:51:19 +0900

    Add Mood class

---
+++ app/src/main/java/com/kozirase/app/Mood.java
@@ -0,0 +1,55 @@
+package com.kozirase.app;
+
+public class Mood {
+    private String excite;
+    private String pleasant;
+    private String calm;
+    private String nervous;
+    private String boring;
+    private String unpleasant;
+    private String surprise;
+    private String sleepy;
+
+    public Mood(String excite, String pleasant, String calm, String nervous, String boring, String unpleasant, String surprise, String sleepy) {
+        this.excite = excite;
+        this.pleasant = pleasant;
+        this.calm = calm;
+        this.nervous = nervous;
+        this.boring = boring;
+        this.unpleasant = unpleasant;
+        this.surprise = surprise;
+        this.sleepy = sleepy;
+    }
+
+    public String getExcite() {
+        return excite;
+    }
+
+    public String getPleasant() {
+        return pleasant;
+    }
+
+    public String getCalm() {
+        return calm;
+    }
+
+    public String getNervous() {
+        return nervous;
+    }
+
+    public String getBoring() {
+        return boring;
+    }
+
+    public String getUnpleasant() {
+        return unpleasant;
+    }
+
+    public String getSurprise() {
+        return surprise;
+    }
+
+    public String getSleepy() {
+        return sleepy;
+    }
+}
