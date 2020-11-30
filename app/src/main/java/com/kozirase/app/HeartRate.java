7b37066bec6c4133b713b595144b832a6795b882
Bryanwangzb <bryanwzb@163.com>
2020-11-30 20:27:23 +0900

    definite class HeartRate contents

--- app/src/main/java/com/kozirase/app/HeartRate.java
+++ app/src/main/java/com/kozirase/app/HeartRate.java
@@ -1,5 +1,13 @@
 package com.kozirase.app;
 
 public class HeartRate {
+    // variable name same as in heart rate data
+    private String dateTime;
+    private Value value;
+
+    public class Value {
+        private int bpm;
+        private int confidence;
+    }
 
 }
