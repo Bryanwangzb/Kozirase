89b1b586c159fde07b90bd197d67827c72c0c0d1
Bryanwangzb <bryanwzb@163.com>
2020-12-06 21:41:02 +0900

    implete getting json file's all contents to list.

--- app/src/main/java/com/kozirase/app/HeartRateProcessActivity.java
+++ app/src/main/java/com/kozirase/app/HeartRateProcessActivity.java
@@ -3,10 +3,18 @@ package com.kozirase.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.List;
 
 public class HeartRateProcessActivity extends AppCompatActivity {
+    List<String> timeList;
+    List<Integer> heartRatesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,22 +24,36 @@ public class HeartRateProcessActivity extends AppCompatActivity {
         //HeartRate heartRate = new HeartRate("07/28/20 15:57:27",new HeartRate.Value(70,2));
         //String json = gson.toJson(heartRate);
 
+        String jsonFileString = Utils.getJsonFromAssets(getApplicationContext(), "heart_rate-2020-07029.json");
+        //Log.i("data", jsonFileString);
         Gson gson = new Gson();
+        Type listUserType = new TypeToken<List<HeartRate>>() {
+        }.getType();
 
-        String json = "{\n" +
-                "  \"dateTime\" : \"07/28/20 15:56:42\",\n" +
-                "  \"value\" : {\n" +
-                "    \"bpm\" : 70,\n" +
-                "    \"confidence\" : 0\n" +
-                "  }\n" +
-                "}";
+        List<HeartRate> heartRates = gson.fromJson(jsonFileString, listUserType);
+        System.out.println("The item's amount is:" + heartRates.size());
 
-        HeartRate heartRate = gson.fromJson(json, HeartRate.class);
-        // For testing gson function.
-        System.out.println("*********************************");
-        System.out.println(heartRate.getDateTime());
-        System.out.println(heartRate.getValue().getBpm());
-        System.out.println(heartRate.getValue().getConfidence());
+//        for (int i = 0; i < heartRates.size(); i++) {
+//            Log.i("data", "> Item" + i + "\n" +
+//                    "Time: " + heartRates.get(i).getDateTime() + "\n" +
+//                    "HeartRate: " + heartRates.get(i).getValue().getBpm() + "\n");
+//        }
+
+//        String json = "{\n" +
+//                "  \"dateTime\" : \"07/28/20 15:56:42\",\n" +
+//                "  \"value\" : {\n" +
+//                "    \"bpm\" : 70,\n" +
+//                "    \"confidence\" : 0\n" +
+//                "  }\n" +
+//                "}";
+//
+//        HeartRate heartRate = gson.fromJson(json, HeartRate.class);
+//        // For testing gson function.
+//        System.out.println("*********************************");
+//        System.out.println(heartRate.getDateTime());
+//        System.out.println(heartRate.getValue().getBpm());
+//        System.out.println(heartRate.getValue().getConfidence());
+
 
     }
 }
--- app/src/main/java/com/kozirase/app/Utils.java
+++ app/src/main/java/com/kozirase/app/Utils.java
@@ -1,14 +1,18 @@
 package com.kozirase.app;
 
 import android.content.Context;
-
+import android.content.res.AssetManager;
+import android.util.Log;
 import java.io.IOException;
 import java.io.InputStream;
 
 public class Utils {
-    static String getJsonFromAssets(Context context, String fileName){
+
+    static String getJsonFromAssets(Context context, String fileName) {
         String jsonString;
-        try{
+
+
+        try {
             InputStream is = context.getAssets().open(fileName);
 
             int size = is.available();
@@ -16,11 +20,12 @@ public class Utils {
             is.read(buffer);
             is.close();
 
-            jsonString = new String(buffer,"UTF-8");
+            jsonString = new String(buffer, "UTF-8");
         } catch (IOException e) {
             e.printStackTrace();
             return null;
         }
+
         return jsonString;
     }
 }
