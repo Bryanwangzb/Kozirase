5d01d706f3e66e079b05f60146398642a11c0941
Bryanwangzb <bryanwzb@163.com>
2020-12-07 11:46:41 +0900

    add getting the date in milli seconds.

--- app/src/main/java/com/kozirase/app/HeartRateProcessActivity.java
+++ app/src/main/java/com/kozirase/app/HeartRateProcessActivity.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -13,25 +17,51 @@ import java.lang.reflect.Type;
 import java.util.List;
 
 public class HeartRateProcessActivity extends AppCompatActivity {
+    private LineChart heartRateLineChart;
     List<String> timeList;
     List<Integer> heartRatesList;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heart_rate_process);
 
+        initViews();
+
+        for(int i =0;i<10;i++){
+            timeList.add((String.valueOf(i)));
+            heartRatesList.add(i*i);
+        }
+
+        XAxis xAxis = heartRateLineChart.getXAxis();
+        YAxis leftAxis = heartRateLineChart.getAxisLeft();
+
+        XAxis.XAxisPosition position = XAxis.XAxisPosition.BOTTOM;
+        xAxis.setPosition(position);
+
+        heartRateLineChart.getDescription().setEnabled(true);
+        Description description = new Description();
+        description.setText("Heart Rate");
+        description.setTextSize(15f);
+
+
+
+
         //HeartRate heartRate = new HeartRate("07/28/20 15:57:27",new HeartRate.Value(70,2));
         //String json = gson.toJson(heartRate);
 
-        String jsonFileString = Utils.getJsonFromAssets(getApplicationContext(), "heart_rate-2020-07029.json");
+        //String jsonFileString = Utils.getJsonFromAssets(getApplicationContext(), "heart_rate-2020-07029.json");
         //Log.i("data", jsonFileString);
-        Gson gson = new Gson();
-        Type listUserType = new TypeToken<List<HeartRate>>() {
-        }.getType();
+        //Gson gson = new Gson();
+        //Type listUserType = new TypeToken<List<HeartRate>>() {
+        //}.getType();
 
-        List<HeartRate> heartRates = gson.fromJson(jsonFileString, listUserType);
-        System.out.println("The item's amount is:" + heartRates.size());
+        //List<HeartRate> heartRates = gson.fromJson(jsonFileString, listUserType);
+        //System.out.println("The item's amount is:" + heartRates.size());
+
+
 
 //        for (int i = 0; i < heartRates.size(); i++) {
 //            Log.i("data", "> Item" + i + "\n" +
@@ -39,14 +69,6 @@ public class HeartRateProcessActivity extends AppCompatActivity {
 //                    "HeartRate: " + heartRates.get(i).getValue().getBpm() + "\n");
 //        }
 
-//        String json = "{\n" +
-//                "  \"dateTime\" : \"07/28/20 15:56:42\",\n" +
-//                "  \"value\" : {\n" +
-//                "    \"bpm\" : 70,\n" +
-//                "    \"confidence\" : 0\n" +
-//                "  }\n" +
-//                "}";
-//
 //        HeartRate heartRate = gson.fromJson(json, HeartRate.class);
 //        // For testing gson function.
 //        System.out.println("*********************************");
@@ -55,5 +77,12 @@ public class HeartRateProcessActivity extends AppCompatActivity {
 //        System.out.println(heartRate.getValue().getConfidence());
 
 
+    }
+
+    private void initViews(){
+        //TODO: init views here
+        heartRateLineChart = findViewById(R.id.lineChartHeartRate);
+        heartRateLineChart.setTouchEnabled(true);
+        heartRateLineChart.setPinchZoom(true);
     }
 }
--- app/src/main/java/com/kozirase/app/Utils.java
+++ app/src/main/java/com/kozirase/app/Utils.java
@@ -5,6 +5,10 @@ import android.content.res.AssetManager;
 import android.util.Log;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class Utils {
 
@@ -27,5 +31,19 @@ public class Utils {
         }
 
         return jsonString;
+    }
+
+    public static long getDataInMilliSeconds(String givenDateString, String format){
+        String DATE_TIME_FORMAT = format;
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_TIME_FORMAT, Locale.JAPAN);
+        long timeInMilliseconds = 1;
+        try{
+            Date mDate = sdf.parse(givenDateString);
+            timeInMilliseconds = mDate.getTime();
+        } catch (ParseException e) {
+            e.printStackTrace();
+
+        }
+        return timeInMilliseconds;
     }
 }
