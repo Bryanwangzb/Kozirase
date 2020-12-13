79f18a5a196122aee06a2d47d20d7faee7893455
woodymeter <woodymeter@gmail.com>
2020-12-13 13:11:30 +0900

    showlaveVoltage changed.

--- app/src/main/java/com/kozirase/app/FinalScoreActivity.java
+++ app/src/main/java/com/kozirase/app/FinalScoreActivity.java
@@ -10,25 +10,26 @@ import android.widget.ImageView;
 
 public class FinalScoreActivity extends AppCompatActivity {
     ImageView img;
-    int loveVoltage = 50;
-
+    int loveVoltage = 100;   //50;              // テスト用に変えてみる
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final_score);
 
+        img = ((ImageView)findViewById(R.id.smallHeatMark)); // img view オブジェクトを取得しておく
+        showloveVoltage();    // 追加。これがなければshowloveVoltageは呼ばれない
+    }
 
-        public void showloveVoltage(View v){
-            if (loveVoltage >= 80)
-                img.setImageResource(R.drawable.bigheat);
-            else (loveVoltage >= 50)
-                img.setImageResource(R.drawable.middleheat);
-            else
-                img.setImageResource(R.drawable.smallheat);
-
-
+    public void showloveVoltage(){                                       // view parameter は削除
+        if (loveVoltage >= 80) {
+            img.setImageResource(R.drawable.bigheat);
         }
-
+        else if (loveVoltage >= 50) {                                    // if else の ネスト{} を修正
+            img.setImageResource(R.drawable.middleheat);
+        }
+        else {
+            img.setImageResource(R.drawable.smallheat);
+        }
     }
 }
