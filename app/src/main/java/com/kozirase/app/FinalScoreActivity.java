a9f281e9d4f4fd78278cbc2cab5e02a8825ad9c8
OsakiMiku <miku.osaki@ibm.com>
2020-12-13 11:29:49 +0900

    add TextView Object

--- app/src/main/java/com/kozirase/app/FinalScoreActivity.java
+++ app/src/main/java/com/kozirase/app/FinalScoreActivity.java
@@ -2,13 +2,44 @@ package com.kozirase.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.widget.ImageView;
 
 public class FinalScoreActivity extends AppCompatActivity {
+    ImageView img;
+    int loveVoltage = 50;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final_score);
+
+        img = findViewByid(R.id.smallHeatMark);
+
+        public void showloveVoltage(View v){
+            if (loveVoltage >= 80)
+                img.setImageResource(R.drawable.bigHeat);
+            else (loveVoltage >= 50)
+                img.setImageResource(R.drawable.middleHeat);
+            else
+                img.setImageResource(R.drawable.smallHeat);
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
     }
 }
