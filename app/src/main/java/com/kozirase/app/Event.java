8f3dc18f774ba7e7a9caf25c8d694ed037c909a1
Bryanwangzb <bryanwzb@163.com>
2021-01-14 16:54:18 +0900

    add Event class.

---
+++ app/src/main/java/com/kozirase/app/Event.java
@@ -0,0 +1,65 @@
+package com.kozirase.app;
+
+import androidx.room.PrimaryKey;
+
+public class Event {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String eventName;
+    private String firstMember;
+    private String secondMember;
+    private String thirdMember;
+    private String fourthMember;
+
+    public Event(String eventName, String firstMember, String secondMember, String thirdMember, String fourthMember) {
+        this.eventName = eventName;
+        this.firstMember = firstMember;
+        this.secondMember = secondMember;
+        this.thirdMember = thirdMember;
+        this.fourthMember = fourthMember;
+    }
+
+    public void setId(int id){this.id = id;}
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getFirstMember() {
+        return firstMember;
+    }
+
+    public void setFirstMember(String firstMember) {
+        this.firstMember = firstMember;
+    }
+
+    public String getSecondMember() {
+        return secondMember;
+    }
+
+    public void setSecondMember(String secondMember) {
+        this.secondMember = secondMember;
+    }
+
+    public String getThirdMember() {
+        return thirdMember;
+    }
+
+    public void setThirdMember(String thirdMember) {
+        this.thirdMember = thirdMember;
+    }
+
+    public String getFourthMember() {
+        return fourthMember;
+    }
+
+    public void setFourthMember(String fourthMember) {
+        this.fourthMember = fourthMember;
+    }
+}
