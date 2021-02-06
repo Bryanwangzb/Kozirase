04f603eee24fa8c1f23af2ccc6e6f0d9a324a0a6
hinamgr <hinamgr@gmail.com>
2021-02-06 12:57:02 +0900

    Fixed score page and text input page in the meeting.

--- app/src/main/java/com/kozirase/app/LoginActivity.java
+++ app/src/main/java/com/kozirase/app/LoginActivity.java
@@ -1,7 +1,19 @@
 package com.kozirase.app;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
-public class LoginActivity extends Activity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import java.util.List;
+
+public class LoginActivity extends AppCompatActivity {
 
 }
