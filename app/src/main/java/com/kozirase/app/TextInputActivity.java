47a4781d5ec4f33945ed0dc8ec65c6f4b68112d2
Bryanwangzb <bryanwzb@163.com>
2021-02-06 15:20:18 +0900

    delete constraint comment in linear layout.

--- app/src/main/java/com/kozirase/app/TextInputActivity.java
+++ app/src/main/java/com/kozirase/app/TextInputActivity.java
@@ -1,33 +1,35 @@
 package com.kozirase.app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class TextInputActivity extends AppCompatActivity {
 
-    private Button btnSaveText;
+    //Todo: update this activity with kozirase API.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_input);
-
-        initViews();
-
-        btnSaveText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(TextInputActivity.this, FinalScoreActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
-    private void initViews() {
-        btnSaveText = findViewById(R.id.button_to_score_page);
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
+
 }
