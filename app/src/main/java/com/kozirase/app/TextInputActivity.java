ad8b0cf94d2f10a2da37c98c90df40302e1e62c7
Bryanwangzb <bryanwzb@163.com>
2021-02-13 17:37:37 +0900

    add new views for api preparation.

--- app/src/main/java/com/kozirase/app/TextInputActivity.java
+++ app/src/main/java/com/kozirase/app/TextInputActivity.java
@@ -2,29 +2,41 @@ package com.kozirase.app;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.ItemTouchHelper;
 
-import android.app.ActionBar;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.Objects;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class TextInputActivity extends AppCompatActivity {
 
     //Todo: update this activity with kozirase API.
+    //Todo: expect dynamically.
+
+    private Button btnGetApiFirstMember, btnGetApiSecondMember;
+    private EditText editTextInputFirstMember, editTextInputSecondMember;
+    private JsonMoodScoreApi jsonMoodScoreApi;
+    private String inputContextFirstMember, inputContextSecondMember;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_input);
-
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        initView();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://kojipro.an.r.appsot.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
     }
-
-
 
 
     @Override
@@ -37,6 +49,14 @@ public class TextInputActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private void initView() {
+        btnGetApiFirstMember = findViewById(R.id.btn_text_analyst_person_1);
+        btnGetApiSecondMember = findViewById(R.id.btn_text_analyst_person_2);
+        editTextInputFirstMember = findViewById(R.id.putText_Person_1);
+        editTextInputSecondMember = findViewById(R.id.putText_Person_2);
     }
 
 }
