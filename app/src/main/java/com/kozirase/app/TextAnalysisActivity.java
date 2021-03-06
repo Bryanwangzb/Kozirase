c44b6d501522644d4ebc458b08a6330a43aa7a7a
Bryanwangzb <bryanwzb@163.com>
2021-03-06 22:45:08 +0900

    add test button in main activity.

--- app/src/main/java/com/kozirase/app/TextAnalysisActivity.java
+++ app/src/main/java/com/kozirase/app/TextAnalysisActivity.java
@@ -1,14 +1,35 @@
 package com.kozirase.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class TextAnalysisActivity extends AppCompatActivity {
+
+    private TextAnalysisViewModel textAnalysisViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_analysis);
+
+        textAnalysisViewModel = new ViewModelProvider(this).get(TextAnalysisViewModel.class);
+        textAnalysisViewModel.getAllTextAnalysis().observe(this, new Observer<List<TextAnalysis>>() {
+
+            // this function is triggered when contents in Live data has changed.
+            @Override
+            public void onChanged(List<TextAnalysis> textAnalyses) {
+                //update RecyclerView
+                Toast.makeText(TextAnalysisActivity.this,"",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
+
 }
