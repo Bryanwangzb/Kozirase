cd0663e124c5a9ca1168dc4704ea63670d97a985
Bryanwangzb <bryanwzb@163.com>
2021-03-06 21:53:20 +0900

    add text analysis repository.

--- app/src/main/java/com/kozirase/app/TextAnalysisDatabase.java
+++ app/src/main/java/com/kozirase/app/TextAnalysisDatabase.java
@@ -1,10 +1,13 @@
 package com.kozirase.app;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = {TextAnalysis.class},version = 1)
 public abstract class TextAnalysisDatabase extends RoomDatabase {
@@ -18,8 +21,34 @@ public abstract class TextAnalysisDatabase extends RoomDatabase {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     TextAnalysisDatabase.class,"text_analysis_database")
                     .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)  // run callback and populate db task.
                     .build();
         }
         return instance;
+    }
+
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback(){
+        // the onCreate method only runs when creating the database, while the open runs every time when opening database
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void>{
+        private TextAnalysisDao textAnalysisDao;
+
+        private PopulateDbAsyncTask(TextAnalysisDatabase db){
+            textAnalysisDao = db.textAnalysisDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            textAnalysisDao.insert(new TextAnalysis("Tom","It's a nice day!","Happy"));
+            textAnalysisDao.insert(new TextAnalysis("Jack","It's a normal day!","Well"));
+            textAnalysisDao.insert(new TextAnalysis("Sam","It's a bad day!","Sad"));
+            return null;
+        }
     }
 }
--- app/src/main/java/com/kozirase/app/TextAnalysisRepository.java
+++ app/src/main/java/com/kozirase/app/TextAnalysisRepository.java
@@ -1,6 +1,7 @@
 package com.kozirase.app;
 
 import android.app.Application;
+import android.os.AsyncTask;
 
 import androidx.lifecycle.LiveData;
 
@@ -16,25 +17,84 @@ public class TextAnalysisRepository {
         allTextAnalysis = textAnalysisDao.getAllTextAnalysis();
     }
 
+    // these method are APIs for ViewModel
     public void insert(TextAnalysis textAnalysis){
-
+        new InsertTextAnalysisAsyncTask(textAnalysisDao).execute(textAnalysis);
     }
 
     public void update(TextAnalysis textAnalysis){
-
+        new UpdateTextAnalysisAsyncTask(textAnalysisDao).execute(textAnalysis);
     }
 
     public void delete(TextAnalysis textAnalysis){
-
+        new DeleteTextAnalysisAsyncTask(textAnalysisDao).execute(textAnalysis);
     }
 
     public void deleteAllTextAnalysis(){
-
+        new DeleteAllTextAnalysisAsyncTask(textAnalysisDao).execute();
     }
 
     public LiveData<List<TextAnalysis>> getAllTextAnalysis(){
         return allTextAnalysis;
     }
+
+    private static class InsertTextAnalysisAsyncTask extends AsyncTask<TextAnalysis,Void,Void>{
+        private TextAnalysisDao textAnalysisDao;
+
+        // Constructor
+        private InsertTextAnalysisAsyncTask(TextAnalysisDao textAnalysisDao){
+            this.textAnalysisDao = textAnalysisDao;
+        }
+
+        @Override
+        protected Void doInBackground(TextAnalysis... textAnalyses) {
+            textAnalysisDao.insert(textAnalyses[0]);
+            return null;
+        }
+    }
+
+    private static class UpdateTextAnalysisAsyncTask extends AsyncTask<TextAnalysis,Void,Void>{
+        private TextAnalysisDao textAnalysisDao;
+
+        private UpdateTextAnalysisAsyncTask(TextAnalysisDao textAnalysisDao){
+            this.textAnalysisDao = textAnalysisDao;
+        }
+
+        @Override
+        protected Void doInBackground(TextAnalysis... textAnalyses) {
+            textAnalysisDao.update(textAnalyses[0]);
+            return null;
+        }
+    }
+
+    private static class DeleteTextAnalysisAsyncTask extends AsyncTask<TextAnalysis,Void,Void>{
+        private TextAnalysisDao textAnalysisDao;
+
+        private DeleteTextAnalysisAsyncTask(TextAnalysisDao textAnalysisDao){
+            this.textAnalysisDao = textAnalysisDao;
+        }
+
+        @Override
+        protected Void doInBackground(TextAnalysis... textAnalyses) {
+            textAnalysisDao.delete(textAnalyses[0]);
+            return null;
+        }
+    }
+
+    private static class DeleteAllTextAnalysisAsyncTask extends AsyncTask<Void,Void,Void>{
+        private TextAnalysisDao textAnalysisDao;
+
+        private DeleteAllTextAnalysisAsyncTask(TextAnalysisDao textAnalysisDao){
+            this.textAnalysisDao = textAnalysisDao;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            textAnalysisDao.deleteAllTextAnalysis();
+            return null;
+        }
+    }
+
 
 
 
