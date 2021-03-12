44facfb7152f7734eec4717cb41eb4182c20b905
Bryanwangzb <bryanwzb@163.com>
2021-03-12 22:40:51 +0900

    add mood api to text analysis activity.

--- app/src/main/java/com/kozirase/app/AddTextAnalysisActivity.java
+++ app/src/main/java/com/kozirase/app/AddTextAnalysisActivity.java
@@ -7,16 +7,32 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class AddTextAnalysisActivity extends AppCompatActivity {
 
     private EditText editTextMember;
     private EditText editTextDialogue;
     private TextView textViewTextAnalysisResult;
+    private Button btnGetApi;
+    private JsonMoodScoreApi jsonMoodScoreApi;
+    private String textAnalysisContext;
 
 
 
@@ -29,6 +45,20 @@ public class AddTextAnalysisActivity extends AppCompatActivity {
         editTextMember = findViewById(R.id.edit_text_analysis_member_name);
         editTextDialogue = findViewById(R.id.edit_text_analysis_dialogue);
         textViewTextAnalysisResult = findViewById(R.id.text_text_analysis_result);
+        btnGetApi = findViewById(R.id.btn_text_analyst);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://kojipro.an.r.appspot.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        jsonMoodScoreApi = retrofit.create(JsonMoodScoreApi.class);
+        btnGetApi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getMoods();
+            }
+        });
 
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
         setTitle("Add Text Analysis");
@@ -67,6 +97,48 @@ public class AddTextAnalysisActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+
+    }
+
+    private void getMoods(){
+        Map<String,String> parameters = new HashMap<>();
+        textAnalysisContext = editTextDialogue.getText().toString();
+        parameters.put("text",textAnalysisContext);
+
+        Call<Mood> call = jsonMoodScoreApi.getMoods(parameters);
+
+        call.enqueue(new Callback<Mood>() {
+            @Override
+            public void onResponse(Call<Mood> call, @NotNull Response<Mood> response) {
+                if(!response.isSuccessful()){
+                    textViewTextAnalysisResult.setText("Code: " + response.code());
+                    return;
+                }
+
+                Mood mood = response.body();
+
+                String content = "";
+                content += "Excite: " + mood.getExcite() + "\n";
+                content += "Pleasant: " + mood.getPleasant() + "\n";
+                content += "Calm: " + mood.getCalm() + "\n";
+                content += "Nervous: " + mood.getNervous() + "\n";
+                content += "Boring: " + mood.getBoring() + "\n";
+                content += "Unpleasant: " + mood.getUnpleasant() + "\n";
+                content += "Surprise: " + mood.getSurprise() + "\n";
+                content += "Sleepy: " + mood.getSleepy() + "\n";
+                content += "Myakuari: " + mood.getMyakuari() + "\n\n";
+
+                textViewTextAnalysisResult.setText("");
+                textViewTextAnalysisResult.append(content);
+
+
+            }
+
+            @Override
+            public void onFailure(Call<Mood> call, Throwable t) {
+                textViewTextAnalysisResult.setText(t.getMessage());
+            }
+        });
 
     }
 }
